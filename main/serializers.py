a0d438f94e89472352f730b9e0951b1a6ec7d84d
Murgany <76653393+Murgany@users.noreply.github.com>
2024-03-24 14:57:16 +0200

    Update serializers.py
    Added documentation to serializers

--- main/serializers.py
+++ main/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
 from .models import StudentInfo, StudentClassInfo, Attendance
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django import forms
-from django.contrib.auth.models import Group
-
 
 class StudentInfoSerializer(serializers.ModelSerializer):
-    
+    """
+    Serializer for StudentInfo model.
+    """
+    # Customizing admission_date field widget
     admission_date = forms.DateField(widget=forms.DateInput(format='%m/%d%Y'))
- 
+
     class Meta:
         model = StudentInfo
         fields = ['id', 'name', 'age', 'class_type', 'gender', 'section', 'shift_name', 'admission_date', 'academic_year', 
@@ -17,30 +18,45 @@ class StudentInfoSerializer(serializers.ModelSerializer):
 
 
 class StudentClassInfoSerializer(serializers.ModelSerializer):
+    """
+    Serializer for StudentClassInfo model.
+    """
     class Meta:
         model = StudentClassInfo
-        fields = ['class_name', 'class_short_form', 'class_count', ]
+        fields = ['class_name', 'class_short_form', 'class_count']
 
 
 class AttendanceSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Attendance model.
+    """
     class Meta:
         model = Attendance
         fields = ['student', 'date', 'status']
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for User model.
+    """
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user registration.
+    """
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        """
+        Create a new user.
+        """
         user = User.objects.create_user(
             validated_data['username'],
             validated_data['email'],
@@ -48,7 +64,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
 
-class GroupSerializer(serializers.ModelSerializer):    
+class GroupSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Group model.
+    """
     class Meta:
         model = Group
         fields = ["name"]
